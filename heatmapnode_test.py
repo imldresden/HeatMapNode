5f299febc38bc7ebeb2da09e0d27026241a45f1c
Lucas Recknagel <lucas91@me.com>
2016-07-27 13:13:17 +0200

    Added minimal python testfile which load plugin and init a HeatMapNode

---
+++ heatmapnode_test.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from libavg import avg, app, player
+
+player.loadPlugin('heatmap')
+
+class HeatMapDiv(app.MainDiv):
+    def onInit(self):
+        self.matrix_node = heatmap.HeatMapNode(parent=self, size=self.size)
+        print self.matrix_node
+        print self.matrix_node.info
+
+resolution = avg.Point2D(800,600)
+resolutionStr = str(resolution.x) + ", " + str(resolution.y)
+app.App().run(HeatMapDiv(), app_resolution=resolutionStr)
