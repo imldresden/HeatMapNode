8cd2884e284e232f24877fbaf1a67d3934574a21
Lucas Recknagel <lucas91@me.com>
2016-08-02 12:35:41 +0200

    Adapted python minimal example to new types

--- heatmapnode_test.py
+++ heatmapnode_test.py
@@ -7,7 +7,23 @@ player.loadPlugin('heatmap')
 
 class HeatMapDiv(app.MainDiv):
     def onInit(self):
-        self.matrix_node = heatmap.HeatMapNode(parent=self, size=self.size)
+
+        matrix = [[1.0,2.0,3.0,4.0],[5.0,6.0,7.0,8.0],[9.0,10.0,11.0,12.0]]
+
+        cm = ["00b159","d11141"]
+
+        self.heatmap_node = heatmap.HeatMapNode(
+            parent=self,
+            size=self.size,
+            viewportrangemin=(0,0),
+            viewportrangemax=(1,1),
+            mapsize=(10,10),
+            valuerangemin=0.0,
+            valuerangemax=50.0,
+            colormap=cm
+        )
+        self.heatmap_node.setMatrix(matrix)
+        print (self.heatmap_node.viewportrangemin, self.heatmap_node.viewportrangemax, self.heatmap_node.mapsize, self.heatmap_node.valuerangemin, self.heatmap_node.valuerangemax, self.heatmap_node.colormap)
 
 resolution = avg.Point2D(800,600)
 resolutionStr = str(resolution.x) + ", " + str(resolution.y)
